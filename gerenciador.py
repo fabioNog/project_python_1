88896f79c9f626d364fc5524d04dbcb3a6142434
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2024-06-09 11:41:17 -0300

    Menu While

--- gerenciador.py
+++ gerenciador.py
@@ -1,13 +1,17 @@
-print("\n Menu do Gerenciador")
+while True:
 
-print("1. Adiconar Tarefas")
-print("2. Ver Tarefas")
-print("3. Atualizar Tarefas")
-print("4. Completar Tarefas")
-print("5. Deletar Tarefas Completadas")
-print("6. Sair")
+    print("\n Menu do Gerenciador")
 
-escolha = input("Digite a sua escolha: ")
+    print("1. Adiconar Tarefas")
+    print("2. Ver Tarefas")
+    print("3. Atualizar Tarefas")
+    print("4. Completar Tarefas")
+    print("5. Deletar Tarefas Completadas")
+    print("6. Sair")
 
-if escolha == "6":
-    print("Programa Finalizado")
+    escolha = input("Digite a sua escolha: ")
+
+    if escolha == "6":        
+        break
+    
+print("Programa Finalizado")
