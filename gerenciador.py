b4cf9fa9228b24042b7adf92daf040224c454c50
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2024-06-09 11:36:15 -0300

    Build Menu

---
+++ gerenciador.py
@@ -0,0 +1,13 @@
+print("\n Menu do Gerenciador")
+
+print("1. Adiconar Tarefas")
+print("2. Ver Tarefas")
+print("3. Atualizar Tarefas")
+print("4. Completar Tarefas")
+print("5. Deletar Tarefas Completadas")
+print("6. Sair")
+
+escolha = input("Digite a sua escolha: ")
+
+if escolha == "6":
+    print("Programa Finalizado")
