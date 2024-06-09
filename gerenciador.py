7360602ecfefbd1562572eab8d2f0fd3ae4c9b0c
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2024-06-09 12:18:44 -0300

    First function

--- gerenciador.py
+++ gerenciador.py
@@ -1,3 +1,11 @@
+def adicionar_tarefa(tarefas,nome_tarefa):
+    tarefa = {"nome": nome_tarefa,"completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    return 
+
+tarefas = []
+
 while True:
 
     print("\n Menu do Gerenciador")
@@ -10,8 +18,10 @@ while True:
     print("6. Sair")
 
     escolha = input("Digite a sua escolha: ")
-
-    if escolha == "6":        
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")        
+        adicionar_tarefa(tarefas,nome_tarefa)
+    elif escolha == "6":        
         break
     
 print("Programa Finalizado")
