71d735600535fe8d4d4b385351ee753b2e47da65
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-06-10 21:28:04 -0300

    view tarefas

--- gerenciador.py
+++ gerenciador.py
@@ -2,7 +2,14 @@ def adicionar_tarefa(tarefas,nome_tarefa):
     tarefa = {"nome": nome_tarefa,"completada": False}
     tarefas.append(tarefa)
     print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
-    return 
+    return
+
+def ver_tarefas(tarefas):
+    print("\nLista de tarefas")
+    for indice, tarefa in enumerate(tarefas):
+        status = "✓" if tarefa["completada"] else ""
+        nome_tarefa = tarefa["nome"]
+        print(f"{indice}. [{status}] {nome_tarefa}")
 
 tarefas = []
 
@@ -21,6 +28,10 @@ while True:
     if escolha == "1":
         nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")        
         adicionar_tarefa(tarefas,nome_tarefa)
+
+    elif escolha == "2":
+        ver_tarefas(tarefas)
+
     elif escolha == "6":        
         break
     
