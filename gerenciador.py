a86f1390b11cae008e64b95f959a1b432455ed20
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-08-10 18:08:22 -0300

    Finalizando projeto python

--- gerenciador.py
+++ gerenciador.py
@@ -1,48 +1,68 @@
-def adicionar_tarefa(tarefas,nome_tarefa):
-    tarefa = {"nome": nome_tarefa,"completada": False}
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefa = {"nome": nome_tarefa, "completada": False}
     tarefas.append(tarefa)
-    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    print(f"Tarefa '{nome_tarefa}' foi adicionada com sucesso!")
     return
 
 def ver_tarefas(tarefas):
-    print("\nLista de tarefas")
-    for indice, tarefa in enumerate(tarefas):
-        status = "✓" if tarefa["completada"] else ""
+    print("\nLista de Tarefas:")
+    for indice, tarefa in enumerate(tarefas, start=1):
+        status = "✓" if tarefa["completada"] else " "
         nome_tarefa = tarefa["nome"]
-        print(f"{indice}. atualizada para {nome_tarefa}")
+        print(f"{indice}. [{status}] {nome_tarefa}")
 
-def atualizar_tarefa(tarefas,indice_tarefa,novo_nome_tarefa):
-     print(f"{indice}. [{status}] {nome_tarefa}")
+def atualizar_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+    if 0 < indice_tarefa <= len(tarefas):
+        tarefas[indice_tarefa - 1]["nome"] = novo_nome_tarefa
+        print("Tarefa atualizada com sucesso!")
+    else:
+        print("Índice inválido.")
 
+def completar_tarefa(tarefas, indice_tarefa):
+    if 0 < indice_tarefa <= len(tarefas):
+        tarefas[indice_tarefa - 1]["completada"] = True
+        print("Tarefa marcada como completada!")
+    else:
+        print("Índice inválido.")
+
+def deletar_tarefa(tarefas, indice_tarefa):
+    if 0 < indice_tarefa <= len(tarefas):
+        tarefa_removida = tarefas.pop(indice_tarefa - 1)
+        print(f"Tarefa '{tarefa_removida['nome']}' foi deletada com sucesso!")
+    else:
+        print("Índice inválido.")
 
 tarefas = []
 
-#apenas um comentario
-
 while True:
-
-    print("\n Menu do Gerenciador")
-
-    print("1. Adiconar Tarefas")
+    print("\nMenu do Gerenciador:")
+    print("1. Adicionar Tarefa")
     print("2. Ver Tarefas")
-    print("3. Atualizar Tarefas")
-    print("4. Completar Tarefas")
-    print("5. Deletar Tarefas Completadas")
+    print("3. Atualizar Tarefa")
+    print("4. Completar Tarefa")
+    print("5. Deletar Tarefa")
     print("6. Sair")
 
     escolha = input("Digite a sua escolha: ")
-    if escolha == "1":
-        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")        
-        adicionar_tarefa(tarefas,nome_tarefa)
 
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
     elif escolha == "2":
         ver_tarefas(tarefas)
-    
     elif escolha == "3":
-            atualizar_tarefa(tarefas,indice,novo_nome_tarefa)
-    
-
-    elif escolha == "6":        
+        indice = int(input("Digite o número da tarefa que deseja atualizar: "))
+        novo_nome_tarefa = input("Digite o novo nome da tarefa: ")
+        atualizar_tarefa(tarefas, indice, novo_nome_tarefa)
+    elif escolha == "4":
+        indice = int(input("Digite o número da tarefa que deseja marcar como completada: "))
+        completar_tarefa(tarefas, indice)
+    elif escolha == "5":
+        indice = int(input("Digite o número da tarefa que deseja deletar: "))
+        deletar_tarefa(tarefas, indice)
+    elif escolha == "6":
         break
-    
+    else:
+        print("Escolha inválida. Tente novamente.")
+
 print("Programa Finalizado")
